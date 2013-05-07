0f13cbf54b5dd430b80cc99c51aef09c0a6ff7de
Aaron Wright <acwrightdesign@gmail.com>
2013-05-07 18:21:40 -0500

    Fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,7 @@ Urbanfinchapp::Application.configure do
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     },
-    :url => ':s3_domain_url'
+    :url => ':s3_domain_url',
+    :path => '/:class/:attachment/:id_partition/:style/:filename'
   }
 end
