31693d9afd33ff045cbbaad0917765634c43760b
Aaron Wright <acwrightdesign@gmail.com>
2013-10-22 17:04:33 -0500

    Update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   around_filter :scope_current_account
   
   def not_found
-    render :file => "#{Rails.root}/public/404.html", :status => :not_found
+    render :file => "#{Rails.root}/public/404.html", :layout => false, :status => :not_found
   end
   
   protected
@@ -21,13 +21,17 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   
   def current_account
-    @current_account = Account.find_by(:token => request.subdomain)
+    @current_account ||= Account.where(:token => request.subdomain).first
   end
   helper_method :current_account
   
   def scope_current_account
-    Account.current_id = current_account.id
-    yield
+    if current_account
+      Account.current_id = current_account.id
+      yield
+    else
+      not_found
+    end
   ensure
     Account.current_id = nil
   end
